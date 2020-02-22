5d3f6db51500fafc793110d6f0df580034c799cf
Luis Fernando <fernando04_software@outlook.com>
2020-02-21 19:49:55 -0600

    Update Program.cs & new class "Region"
    Se creo la estructura principal para el manejo de las opciones del juego (menu) y una nueva clase para el manejo de las regiones

--- BattleGame/Program.cs
+++ BattleGame/Program.cs
@@ -5,8 +5,41 @@ namespace BattleGame
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        { 
+            int option;
+
+            do
+            {
+                Console.WriteLine(" Bienvenido a Battle Game");
+                Console.WriteLine("**************************");
+
+                Console.WriteLine(" Menu");
+                Console.WriteLine(" 1.- Jugar \n 2.- Instrucciones \n 3.- Ver estadisticas");
+                Console.WriteLine(" 4.- Registrarse\n 5.- Salir \n 6.-Acerca de");
+
+                Console.Write("Escoge una opcion (número): ");
+                option = Convert.ToInt16(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        Console.WriteLine("Opcion invalida, reintenta.");
+                        break;
+                }
+            } while (option != 5);
+
         }
     }
 }
---
+++ BattleGame/Region.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleGame
+{
+    /// <summary>
+    /// This class have the informacion of each region how
+    /// Countries' name, Region's name, leader.
+    /// </summary>
+    class Region
+    {
+
+        // Countries' region
+        private string[] asia = new string[13] {"Asia","Afganistan", "China", "India", "Irkutsk", "Japon", "Kamchatka", "Oriente Medio",
+                                                "Mongolia", "Siam", "Siberia", "Ural", "Yakutsk"};
+
+        private string[] northAmerica = new string[10] {"America del Norte","Alaska", "Alberta", "Mexico, America Central y el Caribe", 
+                                                        "Estados Unidos del Este", "Groenlandia", "Territorio del Noroeste", 
+                                                        "Ontario", "Quebec", "Estados Unidos del Oeste"};
+
+        private string[] europe = new string[8] {"Europa", "Gran Bretaña", "Islandia", "Europa del Norte", "Escandinavia", "Europa del Sur", 
+                                                    "Ucrania", "Europa Ocidental" };
+
+        private string[] africa = new string[7] {"Africa", "Congo", "Africa Oriental", "Egipto", "Madagascar", "Africa del norte", "Sudafrica" };
+
+        private string[] southAmerica = new string[5] {"America del Sur" ,"Argentina", "Brasil", "Peru", "Venezuela"};
+        private string[] oceania = new string[5] { "Oceania", "Australia Oriental", "Indonesia", "Nueva Guinea", "Australia Occidental" };
+
+
+        /// <summary>
+        /// This method return the countries' number
+        /// in the region
+        /// </summary>
+        /// <param name="region"> Region where search the number of countries</param>
+        /// <returns></returns>
+        public int getNumCountries(string region)
+        {
+            int numCountries = 0;
+
+            if (asia[0].Equals(region))
+                numCountries = asia.Length - 1;
+
+            else if (northAmerica[0].Equals(region))
+                numCountries = northAmerica.Length - 1;
+
+            else if (europe[0].Equals(region))
+                numCountries = europe.Length - 1;
+
+            else if (africa[0].Equals(region))
+                numCountries = europe.Length - 1;
+
+            else if (southAmerica[0].Equals(region))
+                numCountries = southAmerica.Length - 1;
+
+            else if (oceania[0].Equals(region))
+                numCountries = oceania.Length - 1;
+
+            else
+                numCountries = 0;
+
+            return numCountries;
+        }
+
+        public string[] getCountriesRegion(string region)
+        {
+            if (asia[0].Equals(region))
+                return asia;
+
+            else if (northAmerica[0].Equals(region))
+                return northAmerica;
+
+            else if (europe[0].Equals(region))
+                return europe;
+
+            else if (africa[0].Equals(region))
+                return africa;
+
+            else if (southAmerica[0].Equals(region))
+                return southAmerica;
+
+            else if (oceania[0].Equals(region))
+                return oceania;
+        }
+    }
+}
