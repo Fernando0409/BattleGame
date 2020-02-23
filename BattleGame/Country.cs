3457433fd27435c2e185a482ab43462134b610dd
Luis Fernando <fernando04_software@outlook.com>
2020-02-22 22:32:27 -0600

    Create new classes "Country" & "Players"
    Create the class country and player to manage different methods relationed with the countries y players in the game

---
+++ BattleGame/Country.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleGame
+{
+    /// <summary>
+    /// Class control the atributes of each country 
+    /// of game how troops & owner.
+    /// </summary>
+    class Country
+    {
+        private string nameCountry, owner, region;
+        private int troops, reserveForce;
+
+        /// <summary>
+        /// Create a new Country with their initialize features
+        /// </summary>
+        /// <param name="nameCountry">Name of country</param>
+        /// <param name="region">Region or continent of country</param>
+        public Country(string nameCountry, string region)
+        {
+            this.nameCountry = nameCountry;
+            this.region = region;
+            troops = 0;
+        }
+
+
+        /// <summary>
+        /// This method allow change the country
+        /// of owner
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="nameCountry">Country dominated for the player</param>
+        public void setOwner(string player)
+        {
+            owner = player;
+            Console.WriteLine(this.nameCountry + " esta bajo el control de " + owner);
+        }
+
+        /// <summary>
+        /// This method return who are the owner
+        /// of country
+        /// </summary>
+        /// <returns></returns>
+        public string getOwner() => owner;
+
+        /// <summary>
+        /// Return the country's name
+        /// </summary>
+        /// <returns></returns>
+        public string getNameCountry() => nameCountry;
+
+        /// <summary>
+        /// Return the region where is the country
+        /// </summary>
+        /// <returns></returns>
+        public string getRegion() => region;
+
+        /// <summary>
+        /// Add new troops to country in the map
+        /// </summary>
+        /// <param name="troops"></param>
+        public void addTroops(int troops)
+        {
+            this.troops += troops;
+            Console.WriteLine("Se añadieron " + this.troops + " a " + this.nameCountry);
+        }
+
+        /// <summary>
+        /// Delete or reduce the troops in a country
+        /// </summary>
+        /// <param name="troops">Number of troops that reduce the force military
+        /// in the country </param>
+        public void subTroops(int troops)
+        {
+            if (this.troops - troops > 2)
+            {
+                this.troops -= troops;
+                Console.WriteLine(this.nameCountry + "ahora tiene " + this.troops + "tropas");
+            }
+            else
+                Console.WriteLine("Debe haber minimo 2 tropas por region");
+        }
+
+    }
+}
---
+++ BattleGame/Players.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleGame
+{   
+    /// <summary>
+    /// This class manage the atributes and acction of players
+    /// in the game.
+    /// </summary>
+    class Players
+    {
+        private string namePlayer;
+        private int reserveForce, score;
+        private string[] countriesDominated = new string[64];
+
+        /// <summary>
+        /// Create a new player with their initial features
+        /// </summary>
+        /// <param name="namePlayer">Nickname o real name of player</param>
+        public Players(string namePlayer)
+        {
+            this.namePlayer = namePlayer;
+            reserveForce = 0;
+            score = 0;
+        }
+           
+        /// <summary>
+        /// Set the player's name
+        /// </summary>
+        /// <param name="namePlayer"> Player's name of game</param>
+        public void setNamePlayer(string namePlayer)
+        {
+            this.namePlayer = namePlayer;
+            Console.WriteLine("Nombre establecido.");
+        }
+
+        /// <summary>
+        /// Return the player's name
+        /// </summary>
+        public string getNamePlayer() => namePlayer;
+
+        /// <summary>
+        /// Almacenate the player's troops to later place
+        /// in your country
+        /// </summary>
+        /// <param name="troops">Troops to reserve</param>
+        public void setReserveForce(int troops)
+        {
+            reserveForce += troops;
+            Console.WriteLine("Reserva de tropas actualizada");
+        }
+
+        /// <summary>
+        /// Return the number of troops of player
+        /// </summary>
+        /// <returns></returns>
+        public int getReserveForce() => reserveForce;
+
+        /// <summary>
+        /// Add or substract player's score
+        /// </summary>
+        /// <param name="score"></param>
+        public void setScore(int score) => this.score += score;
+
+        /// <summary>
+        /// Return the score that have the player
+        /// in the game
+        /// </summary>
+        /// <returns></returns>
+        public int getScore() => score;
+
+        
+    }
+}
